fc1b0a730c46663d7ddf4b49cb3a2f6fc9cab0b2
Kou <hikkappi@yahoo.co.jp>
2022-05-18 13:37:49 +0900

    fix: TOP画面において目標が登録されていない場合の表記を追加。

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,45 @@
 <template>
   <div>
-    <v-card
-      v-for="target in targetList"
-      :key="target.id"
-      elevation="2"
-      class="mb-3"
+    <div
+      v-if="targetList.length > 0"
     >
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title
-            class="mt-3 mb-3 text-h5"
-          >
-          {{ target.contents }}
-          </v-list-item-title>
-          <div class="text-subtitle-1 mb-2">
-            成果目標
-          </div>
-          <ul
-            v-for="indicator in target.indicators"
-            :key="indicator.id"
-            class="mb-2 text-subtitle-2"
-          >
-            <li>{{ indicator.contents }}</li>
-          </ul>
-        </v-list-item-content>
-      </v-list-item>
-    </v-card>
+      <v-card
+        v-for="target in targetList"
+        :key="target.id"
+        elevation="2"
+        class="mb-3"
+      >
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title
+              class="mt-3 mb-3 text-h5"
+            >
+            {{ target.contents }}
+            </v-list-item-title>
+            <div class="text-subtitle-1 mb-2">
+              成果目標
+            </div>
+            <ul
+              v-for="indicator in target.indicators"
+              :key="indicator.id"
+              class="mb-2 text-subtitle-2"
+            >
+              <li>{{ indicator.contents }}</li>
+            </ul>
+          </v-list-item-content>
+        </v-list-item>
+      </v-card>
+    </div>
+    <div
+      v-else
+    >
+      <v-card
+        elevation="2"
+        class="mb-3"
+      >
+      <v-card-text>目標が登録されておりません。目標を登録しましょう！！</v-card-text>
+      </v-card>
+    </div>
   </div>
 </template>
 
