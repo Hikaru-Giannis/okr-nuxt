e3231943ab24f2ab9ab2b2415960ad8e4560c0a0
Kou <hikkappi@yahoo.co.jp>
2022-06-03 08:09:58 +0900

    fix: 目標ステータスをタグでテキストで表示するように修正。

--- pages/index.vue
+++ pages/index.vue
@@ -11,16 +11,26 @@
       >
         <v-list-item>
           <v-list-item-content>
-            <v-list-item-title
-              class="mt-3 mb-3 text-h5"
+            <div
+              class="d-flex justify-start"
             >
-            {{ target.contents }}
-            </v-list-item-title>
+              <div>
+                <v-list-item-title
+                  class="my-3 text-h5"
+                >
+                {{ target.contents }}
+                </v-list-item-title>
+              </div>
+              <v-chip
+                class="ma-2"
+                color="primary"
+              >
+                {{ target.status }}
+              </v-chip>
+            </div>
+
             <div class="text-subtitle-1 mb-2">
               期限日 : {{ $dateFns.format(new Date(target.expiration_date), 'yyyy-MM-dd') }}まで
-            </div>
-            <div class="text-subtitle-1 mb-2">
-              ステータス : {{ target.status }}
             </div>
             <div class="text-subtitle-1 mb-2">
               成果目標
