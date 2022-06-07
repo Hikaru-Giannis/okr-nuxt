4ced840e15637154914df80cd9fa2728a2a1dff7
Kou <hikkappi@yahoo.co.jp>
2022-06-07 20:08:11 +0900

    add: 評価処理リクエスト時のエラー処理を実装。

--- pages/target/evaluate/_id.vue
+++ pages/target/evaluate/_id.vue
@@ -1,5 +1,10 @@
 <template>
   <div>
+    <SnackBar
+      v-show="isError"
+      :color="'red'"
+      :message="errorMessage"
+    />
     <v-card>
       <v-card-title class="font-weight-bold ml-3 mt-3">
         {{ target.contents }}
@@ -82,105 +87,122 @@
 </template>
 
 <script>
-  export default {
-    name: 'EvaluatePage',
-    async asyncData({ params, $axios, $config }) {
-      const response = await $axios.$get($config.API_BASE_URL + '/api/target/',
-        {
-         params: {
-           target_id: params.id
-         },
-         withCredentials: true
-        }
-      )
-      .catch(err => {
-        return err.response
-      })
+import SnackBar from '@/components/common/SnackBar'
 
-      const target = response.data.target
-
-      return {
-        target
+export default {
+  name: 'EvaluatePage',
+  components: {
+    SnackBar
+  },
+  async asyncData({ params, $axios, $config }) {
+    const response = await $axios.$get($config.API_BASE_URL + '/api/target/',
+      {
+        params: {
+          target_id: params.id
+        },
+        withCredentials: true
       }
+    )
+    .catch(err => {
+      return err.response
+    })
+
+    const target = response.data.target
+
+    return {
+      target
+    }
+  },
+  data() {
+    return {
+      min: 0,
+      max: 1,
+      step: 0.1,
+      isError: false,
+      errorMessage: ''
+    }
+  },
+  computed: {
+    totalScore() {
+      const totalScore = this.target.indicators.reduce(function (accumlator, current) {
+        return accumlator + Number(current.score)
+      }, 0)
+      return totalScore.toFixed(2)
+    }
+  },
+  methods: {
+    async scoreIndicators() {
+      const indicators = this.target.indicators
+      const indicatorsForm = []
+      for (const indicator of indicators) {
+        indicatorsForm.push({
+          indicator_id: indicator.id,
+          score: indicator.score
+        })
+      }
+
+      const response = await this.$axios.$get(this.$config.API_BASE_URL + '/sanctum/csrf-cookie', { withCredentials: true })
+        .then(async () => {
+          return await this.$axios.$post(
+            this.$config.API_BASE_URL + '/api/target/score',
+            {
+              target_id: this.target.id,
+              indicators: indicatorsForm
+            },
+            {
+              withCredentials: true
+            }
+          )
+        })
+        .catch(err => {
+          return err.response
+        })
+
+      if (response.status !== 200) {
+        this.isError = true
+        this.errorMessage = "評価に失敗しました。"
+        return
+      }
+
+      return this.$router.push('/')
     },
-    data() {
-      return {
-        min: 0,
-        max: 1,
-        step: 0.1
+    async completeEvaluate() {
+      const indicators = this.target.indicators
+      const indicatorsForm = []
+      for (const indicator of indicators) {
+        indicatorsForm.push({
+          indicator_id: indicator.id,
+          score: indicator.score
+        })
       }
-    },
-    computed: {
-      totalScore() {
-        const totalScore = this.target.indicators.reduce(function (accumlator, current) {
-          return accumlator + Number(current.score)
-        }, 0)
-        return totalScore.toFixed(2)
+
+      const response = await this.$axios.$get(this.$config.API_BASE_URL + '/sanctum/csrf-cookie', { withCredentials: true })
+        .then(async () => {
+          return await this.$axios.$post(
+            this.$config.API_BASE_URL + '/api/target/complete',
+            {
+              target_id: this.target.id,
+              indicators: indicatorsForm
+            },
+            {
+              withCredentials: true
+            }
+          )
+        })
+        .catch(err => {
+          return err.response
+        })
+
+      if (response.status !== 200) {
+        this.isError = true
+        this.errorMessage = "評価に失敗しました。"
+        return
       }
-    },
-    methods: {
-      async scoreIndicators() {
-        const indicators = this.target.indicators
-        const indicatorsForm = []
-        for (const indicator of indicators) {
-          indicatorsForm.push({
-            indicator_id: indicator.id,
-            score: indicator.score
-          })
-        }
 
-        await this.$axios.$get(this.$config.API_BASE_URL + '/sanctum/csrf-cookie', { withCredentials: true })
-          .then(async (res) => {
-            await this.$axios.$post(
-              this.$config.API_BASE_URL + '/api/target/score',
-              {
-                target_id: this.target.id,
-                indicators: indicatorsForm
-              },
-              {
-                withCredentials: true
-              }
-            )
-          })
-          .catch(err => {
-            return err.response
-          })
-
-        // TODO エラー処理要検討
-        return this.$router.push('/')
-      },
-      async completeEvaluate() {
-        const indicators = this.target.indicators
-        const indicatorsForm = []
-        for (const indicator of indicators) {
-          indicatorsForm.push({
-            indicator_id: indicator.id,
-            score: indicator.score
-          })
-        }
-
-        await this.$axios.$get(this.$config.API_BASE_URL + '/sanctum/csrf-cookie', { withCredentials: true })
-          .then(async (res) => {
-                await this.$axios.$post(
-                  this.$config.API_BASE_URL + '/api/target/complete',
-                  {
-                    target_id: this.target.id,
-                    indicators: indicatorsForm
-                  },
-                  {
-                    withCredentials: true
-                  }
-                )
-              })
-          .catch(err => {
-            return err.response
-          })
-
-        // TODO エラー処理要検討
-        return this.$router.push('/')
-      }
+      return this.$router.push('/')
     }
   }
+}
 </script>
 
 <style>
